266a47e461a5f39eb0f8de33d7ba4f2aef37de9f
sharan8 <sharan.t@u.nus.edu>
2021-03-26 17:08:35 +0800

    Update output display in App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { parse } from './parser/parser'
+import JSONPretty from 'react-json-pretty';
 
 function App() {
-  const [prog, setProg] = useState('default');
+  const [prog, setProg] = useState('1 + 1;;');
+  const [output, setOutput] = useState();
+
+  function displayOutput(obj_output) {
+    console.log(obj_output)
+    setOutput(obj_output)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>OCaml Type Checker</h1>
         <div>
-          <label>
-            Name:
+          <label>Input OCaml code here:</label>
+          <br />
             <input type="text" name="name" value={prog} onChange={e => setProg(e.target.value)}/>
-          </label>
-          <button onClick={() => console.log(parse(prog))}>
+          <button onClick={() => displayOutput(parse(prog))}>
             Submit 
           </button>
+      </div>
+      <br />
+      <label>Output:</label>
+      <div align="left">
+        <h5>Expression Type: {output && output['expression'] ? output['expression'].__proto__.constructor.name : "Unknown"}</h5>
+        <JSONPretty id="json-pretty" data={output}></JSONPretty>
       </div>
       </header>
     </div>
