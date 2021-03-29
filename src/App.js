da801753d2ce6bf250187dae548008b9e4e3fa58
sharan8 <sharan.t@u.nus.edu>
2021-03-29 18:17:12 +0800

    Update frontend with new terminal view

--- src/App.js
+++ src/App.js
@@ -1,47 +1,79 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import { parse } from './parser/parser'
 import { run } from './type-inference/typeInference'
 import { AstError } from './type-inference/errors'
-import JSONPretty from 'react-json-pretty';
+import Terminal from 'terminal-in-react';
 
 function App() {
-  const [prog, setProg] = useState('let a = 5 in fun b -> a;;');
-  const [output, setOutput] = useState();
-
-  function handleClick() {
+  function handleUserInput(userInput) {
     try {
-      const parsed = parse(prog)
+      const parsed = parse(userInput)
+      console.log("Parse output:")
       console.log(parsed)
       const type = run(parsed)
-      setOutput(type)
+      return type.toString()
     } catch (error) {
       if (error instanceof AstError) {
-        setOutput(error)
-      }
-      else {
+        return error.toString()
+      } else {
         throw error
       }
     }
+    
+  }
+
+  const InputTerminal = () => {
+    return (<Terminal
+          color='#89f238'
+          backgroundColor='black'
+          barColor='gray'
+          prompt='white'
+          style={{ fontWeight: "bold", fontSize: "0.7em" }}
+          commands={{
+            'ocaml-help': () => {
+              window.open('https://ocaml.org/learn/taste.html', "_blank")
+              window.focus()
+            },
+            'baby-camels': () => {
+              window.open('https://www.youtube.com/watch?v=o3x_b95KspA', "_blank")
+              window.focus()
+            }
+          }}
+          descriptions={{
+            'ocaml-help': 'open ocaml examples page',
+            'baby-camels': 'easter egg :P'
+          }}
+          commandPassThrough={cmd => handleUserInput(cmd.join())}
+          msg='Input any expression in OCaml below. For help with OCaml, try `ocaml-help`.'
+        />)
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>OCaml Type Checker</h1>
-        <div>
-          <label>Input OCaml code here:</label>
-          <br />
-          <input type="text" name="name" value={prog} onChange={e => setProg(e.target.value)} />
-          <button onClick={handleClick}>
-            Submit
-          </button>
-        </div>
-        <br />
-        <label>Output:</label>
-        <div align="left">
-          <h5>{output && output.toString()}</h5>
-          <JSONPretty id="json-pretty" data={output}></JSONPretty>
+        <div
+          style={{
+            width: "75%"
+          }}
+        >
+          <h1>OCaml Type Checker</h1>
+          <p>Hello there! Welcome to our OCaml type checker based on {' '}
+            <a 
+              href="https://en.wikipedia.org/wiki/Hindley%E2%80%93Milner_type_system" 
+              style={{color:"#ff8080"}}>
+                Hindley-Milner Type Inference (Algorithm W)
+            </a>.
+          </p>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center"
+            }}
+          >
+          <InputTerminal />
         </div>
       </header>
     </div>
